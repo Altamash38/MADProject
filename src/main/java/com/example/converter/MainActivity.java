59ecde49b331718ceed831fa1f7e3cd5152bc00c
Altamash38 <Altamashakber@gmail.com>
2021-08-31 08:49:16 +0500

    First Commit

---
+++ src/main/java/com/example/converter/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+//import com.example.converter.Retrofit.RetrofitBuilder;
+//import com.example.converter.Retrofit.RetroInf;
+import com.google.gson.JsonObject;
+
+import Retrofit.RetroInf;
+import Retrofit.RetrofitBuilder;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText currencyToBeConverted;
+    EditText currencyConverted;
+    Spinner convertToDropdown;
+    Spinner convertFromDropdown;
+    private Object v;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialization of Go-to
+
+        currencyConverted = (EditText) findViewById(R.id.currency_converted);
+        currencyToBeConverted = (EditText) findViewById(R.id.currency_to_be_converted);
+        convertToDropdown = (Spinner) findViewById(R.id.convert_to);
+        convertFromDropdown = (Spinner) findViewById(R.id.convert_from);
+        button = (Button) findViewById(R.id.button);
+
+        String[] dropDownList = {"PKR", "USD", "EUR", "CAD", "INR"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, dropDownList);
+        convertToDropdown.setAdapter(adapter);
+        convertFromDropdown.setAdapter(adapter);
+
+        button.setOnClickListener(new View.OnClickListener());
+    {
+
+        @Override
+            public void onClick(View v){
+                RetroInf retroInf = RetrofitBuilder.getRetrofitInstance().create(RetroInf.class);
+                Call<JsonObject> call = RetroInf.getExchangeCurrency(convertFromDropdown.getSelectedItem().toString());
+                Call.enqueue(new Callback<JsonObject>() {
+                    @Override
+                    public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                        JsonObject res = response.body();
+                        JsonObject rates = res.getAsJsonObject("rates");
+                        double currency = Double.valueOf(currencyToBeConverted.getText().toString());
+                        double multiplier = Double.valueOf(rates.get(convertToDropdown.getSelectedItem().toString()).toString());
+                        double result = currency * multiplier;
+                        currencyConverted.setText(String.valueOf(result));
+                    }
+
+                    @Override
+                    public void onFailure(Call<JsonObject> call, Throwable t) {
+
+                    }
+                })
+            }
+        }};
+}
